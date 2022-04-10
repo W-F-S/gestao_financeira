3c7b2a11cfc1d8d8f684df49ea768da450b90c06
João Pedro <66487538+PJBHL@users.noreply.github.com>
2022-04-10 16:52:24 -0300

    Utils.dart and AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gestor_financeiro/Utils.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
+      home: Scaffold (
+        drawer: const NavigationDrawerWidget(),
+        appBar: AppBar(),
         body: const MyStatelessWidget(),
       ),
     );
